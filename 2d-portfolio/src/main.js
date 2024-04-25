1528ff9bc3a0978f900d20b4bb0e8d14bebfa269
jodyboy <elite.crew66@gmail.com>
2024-04-25 12:34:32 +0700

    map and player movement done

--- 2d-portfolio/src/main.js
+++ 2d-portfolio/src/main.js
@@ -1,6 +1,6 @@
 import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
-import { displayDialogue } from "./utils";
+import { displayDialogue, setCamScale } from "./utils";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
     sliceX: 39,
@@ -82,6 +82,12 @@ k.scene("main", async () => {
         }
     }
 
+    setCamScale(k)
+
+    k.onResize(() => {
+        setCamScale(k);
+    });
+
     k.onUpdate(() => {
         k.camPos(player.pos.x, player.pos.y + 100);
     });
@@ -91,6 +97,31 @@ k.scene("main", async () => {
         
         const worldMousePos = k.toWorld(k.mousePos());
         player.moveTo(worldMousePos, player.speed);
+
+        const mouseAngle = player.pos.angle(worldMousePos)
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if (
+            mouseAngle > lowerBound &&
+            mouseAngle < upperBound &&
+            player.curAnim() !== "walk-up"
+        ) {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+
+        if (
+            mouseAngle < -lowerBound &&
+            mouseAngle > -upperBound &&
+            player.curAnim() !== "walk-down"
+        ) {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+        }
     });
 });
 
